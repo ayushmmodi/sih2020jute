3a04725a022dea416fdc4ce49842241cf0730849
DhairyaSharma7120 <43778496+DhairyaSharma7120@users.noreply.github.com>
2020-08-02 12:37:37 +0530

    d2 commit

--- index.php
+++ index.php
@@ -198,18 +198,18 @@ session_start(); ?>
 <footer style="background: black; padding: 100px;">
   
 
-    <a href="" style="margin-left: 150px; font-size: 35px; text-decoration: none; color: white; ">Gallery</a>
+    <a href="" style="margin-left: 50px; font-size: 25px; text-decoration: none; color: white; ">Gallery</a>
 
-    <a href="" style="margin-left: 250px; font-size: 35px; text-decoration: none; color: white; ">Feedback</a>
-
-
-    <a class="logo-word" style="font-weight: bold;"><a class="text-primary" href="index.php" style="font-size: 50px; margin-left: 200px;">Fiber</a><a href="index.php" style="color: white; font-size: 50px; margin-top: 25px;">Cops</a>
+    <a href="" style="margin-left: 150px; font-size: 25px; text-decoration: none; color: white; ">Feedback</a>
 
 
+    <a class="logo-word" style="font-weight: bold;"><a class="text-primary" href="index.php" style="font-size: 50px; margin-left: 150px;">Fiber</a><a href="index.php" style="color: white; font-size: 50px; margin-top: 25px;">Cops</a>
 
-      <a href="" style="margin-left: 200px; font-size: 35px; text-decoration: none; color: white; ">Gallery</a>
 
-    <a href="" style="margin-left: 250px; font-size: 35px; text-decoration: none; color: white; ">Feedback</a>
+
+      <a href="" style="margin-left: 150px; font-size: 25px; text-decoration: none; color: white; ">Gallery</a>
+
+    <a href="" style="margin-left: 150px; font-size: 25px; text-decoration: none; color: white; ">Feedback</a>
 
 </footer>
 

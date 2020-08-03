1f1b72ee1a6097906ee4ef92a7192a1854e6179f
DhairyaSharma7120 <43778496+DhairyaSharma7120@users.noreply.github.com>
2020-08-03 10:40:58 +0530

    3

--- fiber_cops.sql
+++ fiber_cops.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 03, 2020 at 06:34 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.3.14
+-- Generation Time: Aug 03, 2020 at 07:09 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -29,11 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `user` (
+  `farm_id` int(11) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `mobile_number` varchar(10) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(60) NOT NULL,
   `password` varchar(100) NOT NULL,
   `role` varchar(20) NOT NULL,
   `verify` varchar(50) NOT NULL
@@ -43,12 +44,14 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`first_name`, `last_name`, `email`, `mobile_number`, `username`, `password`, `role`, `verify`) VALUES
-('hihow', 'yoyo', 'ayumodi12@gmail.com', '7383077949', 'ayushmodi12', '$2y$10$ilxxr8PchjINW5VRpHN79uZk3I2mNk5ZNfs4wLQ6M3zyY3LoUaaQK', 'Mill Worker', 'no'),
-('Bhau', 'Gaddi', 'technojar12@gmail.com', '9852146558', 'bhau11', '$2y$10$yA5CU8I08Ehm94/Fh0P/3ulBu/z/jSORpwXFFIPFL6gZ/MJQKbqIa', 'Farmer', 'no'),
-('carry', 'minati', 'edisonmodi@gmail.com', '9852146587', 'edison', '$2y$10$wL8wTNzvL49SPhs73tNzU.ndF9vIruVlERfBLUmbHof.dDsuLP9Pa', 'Farmer', 'no'),
-('dude', 'Modi', 'modiayush98@gmail.com', '9825214658', 'manager', '$2y$10$TiN.5Bis5Y3VXDFhPxyKwOM6.QyH3AYIJsdK.pEG0eNNFrff9YzCC', 'Farmer', 'yes'),
-('abcd', 'xyz', 'ayushmodi12@yahoo.co.in', '9825214658', 'modi', '$2y$10$jfduRadt0NdrGuOiNPUH1ON4zTLnhYtWFlAIojfaPOIUKG03Ig5dC', 'Mill Worker', 'no');
+INSERT INTO `user` (`farm_id`, `first_name`, `last_name`, `email`, `mobile_number`, `username`, `password`, `role`, `verify`) VALUES
+(1, 'hihow', 'yoyo', 'ayumodi12@gmail.com', '7383077949', '', '$2y$10$ilxxr8PchjINW5VRpHN79uZk3I2mNk5ZNfs4wLQ6M3zyY3LoUaaQK', 'Mill Worker', 'no'),
+(2, 'Bhau', 'Gaddi', 'technojar12@gmail.com', '9852146558', '', '$2y$10$yA5CU8I08Ehm94/Fh0P/3ulBu/z/jSORpwXFFIPFL6gZ/MJQKbqIa', 'Farmer', 'no'),
+(3, 'Dhairya', 'Sharma', 'dhairyasharma7120@gmail.com', '8320085121', '', '$2y$10$kdDnM5nut.0DiPM8LZqIvuXsyFRt9fioHfCAu1pxIWjX7amtzB5t6', 'Farmer', 'yes'),
+(4, 'Dhairya', 'Sharma', 'dhairyasharma7120@gmail.com', '8320085121', '', '$2y$10$wRWi3Lsjm4/1o.oxf3C2wOYv3jUAqbZm8Z6/7L7QCJDR2fg3v3Uym', 'Mill Worker', 'no'),
+(5, 'carry', 'minati', 'edisonmodi@gmail.com', '9852146587', '', '$2y$10$wL8wTNzvL49SPhs73tNzU.ndF9vIruVlERfBLUmbHof.dDsuLP9Pa', 'Farmer', 'no'),
+(6, 'dude', 'Modi', 'modiayush98@gmail.com', '9825214658', '', '$2y$10$TiN.5Bis5Y3VXDFhPxyKwOM6.QyH3AYIJsdK.pEG0eNNFrff9YzCC', 'Farmer', 'yes'),
+(7, 'abcd', 'xyz', 'ayushmodi12@yahoo.co.in', '9825214658', '', '$2y$10$jfduRadt0NdrGuOiNPUH1ON4zTLnhYtWFlAIojfaPOIUKG03Ig5dC', 'Mill Worker', 'no');
 
 --
 -- Indexes for dumped tables
@@ -58,7 +61,17 @@ INSERT INTO `user` (`first_name`, `last_name`, `email`, `mobile_number`, `userna
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`farm_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `farm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
